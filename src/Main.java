2691e140c9645a9e7f5ebd45c37c97300243ad72
Neil-Deshpande29 <neildeshpande07@gmail.com>
2025-11-21 14:03:32 -0500

    Made each thread sum 1-1000000

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
 
 public class Main {
 
-
-
     public static void main(String[] args) {
         Thread[] threads = new Thread[1000];
         for(int i=0; i<1000; i++)
         {
-            threads[i] = new Thread();
+            threads[i] = new Thread(()->
+            {
+                long count = 0;
+                for(int n =1; n<=1000000; n++)
+                    count +=n;
+                System.out.println(count);
+            });
+            threads[i].start();
         }
     }
 }
